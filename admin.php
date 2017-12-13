d8d4106cfd43de88d58381f017716d359c2559b1
Gabe Gaerlan <ggaerlan@csumb.edu>
2017-12-13 21:39:04 +0000

    FINISHED

--- admin.php
+++ admin.php
@@ -97,18 +97,12 @@ function userList(){
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <style>
-            @import url("css/styles.css");
-            h1{
-              color:black;
-              font-family: 'Fugaz One', cursive;
-              text-align:center;
-            }
-            
         </style>
     </head>
     <body>
--- delete.php
+++ delete.php
@@ -3,23 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <style>
-            @import url('css/styles.css');
-            .deletePage{
-                text-align:center;
-                font-family: 'Fugaz One', cursive;
-            }
-          body{
-                background-image: url("/finalproject/img/typer.jpg");
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-position: center;
-                text-align:center;
-            }
         </style>
         <script language="javascript">
             function confirmDelete() 
